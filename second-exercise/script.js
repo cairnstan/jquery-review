ce265f8351b023ddfdb108ef497d52dd8a09a88c
cairnstan <cairnsolsen@gmail.com>
2016-09-05 19:19:39 -0500

    Adds comments

--- second-exercise/script.js
+++ second-exercise/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    //adds p element
     $("#one").after("<p>I'm coming after you!</p>");
+    //moves p element to after div two
     $("#two").after($("p"));
+    //removes p element
+    $("p").remove();
 });
